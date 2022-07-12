0e78fda415e53ffd62d33a17b822c73e603ffa0b
Valxer <levanphungk@gmail.com>
2022-07-12 15:17:05 +0200

    repository push

---
+++ SpringBack/src/main/java/repository/CommandeRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.Commande;
+
+public interface CommandeRepository extends JpaRepository<Commande, Integer> {
+
+}
